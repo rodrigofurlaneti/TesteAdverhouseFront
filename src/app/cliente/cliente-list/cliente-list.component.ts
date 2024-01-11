7989c8338e422eb6518831c649c563462d8f3c9c
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2024-01-11 16:58:52 -0300

    Add

--- src/app/cliente/cliente-list/cliente-list.component.ts
+++ src/app/cliente/cliente-list/cliente-list.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteModel } from 'src/interface/cliente.interface';
+import { ClienteListService } from './cliente-list.service';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-cliente-list',
   templateUrl: './cliente-list.component.html',
   styleUrls: ['./cliente-list.component.css']
 })
+
 export class ClienteListComponent implements OnInit {
 
-  constructor() { }
+  //#region [Properties]
+  //Property DisplayedColumns
+  private _displayedColumns: string[] = ['id','name'];
+  get displayedColumns() { return this._displayedColumns; }
+  set displayedColumns(value) { this._displayedColumns = value; }
+
+  //Property DataSource
+  private _dataSource: ClienteModel[] = [];
+  get dataSource() { return this._dataSource; }
+  set dataSource(value) { this._dataSource = value; }
+
+  //Property ClickedRows
+  private _clickedRows = new Set<ClienteModel>();
+  get clickedRows() { return this._clickedRows; }
+  set clickedRows(value) { this._clickedRows = value; }
+  
+  //Property RouterString
+  private _routerString: string = '';
+  get routerString() { return this._routerString; }
+  set routerString(value) { this._routerString = value; }
+
+  //Property MessageErro
+  private _messageErro: string = '';
+  get messageErro() { return this._messageErro; }
+  set messageErro(value) { this._messageErro = value; }
+  
+  // #endregion
+
+  // #region [Constructor]
+
+  constructor(private clienteListService: ClienteListService,
+              private router: Router) { }
+  
+  // #endregion
+
+  // #region [Methods]
 
   ngOnInit(): void {
+    this.list();
   }
+
+  list() {
+    this.messageErro = 'Erro ao listar os clientes!';
+    this.clienteListService.list().subscribe(list => {
+      this.dataSource = list;
+    }, err => {
+      console.log(this.messageErro, err);
+    })
+  }
+
+  reply(){
+    this.routerString = 'main';
+    this.router.navigate([this.routerString]);
+  }
+  // #endregion
 
 }
--- src/app/cliente/cliente-list/cliente-list.service.ts
+++ src/app/cliente/cliente-list/cliente-list.service.ts
@@ -6,19 +6,14 @@ import { ClienteModel } from 'src/interface/cliente.interface';
 @Injectable({
   providedIn: 'root'
 })
-export class BuyerListService {
+export class ClienteListService {
 
-  private route = 'https://localhost:7027/Cliente';
+  private route = 'https://localhost:7016/Cliente';
 
   constructor(private http: HttpClient) { }
 
   list() : Observable<any>{
     var ret = this.http.get(this.route);
-    return ret;
-  }
-
-  listBuyerStatus(id:number) : Observable<any>{
-    var ret = this.http.options<any>(this.route+'/'+id); 
     return ret;
   }
 }
